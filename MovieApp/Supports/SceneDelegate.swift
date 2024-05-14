ff9132de2a584af906b7f28be6d6762e4f58c9a4
ShUlmas <shamuratovolmasbek7@gmail.com>
2024-05-14 16:56:06 +0500

    Add Splash Screen Scene Delegate

--- MovieApp/Supports/SceneDelegate.swift
+++ MovieApp/Supports/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let viewController = TabbarController()
+        let viewController = SplashViewController()
         window.rootViewController = viewController
         self.window = window
         self.window?.makeKeyAndVisible()
